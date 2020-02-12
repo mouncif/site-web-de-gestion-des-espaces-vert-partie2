f09397786a046adeb58f0a3aa16ea81e9007e06d
AbdeslamRehaimi <abdeslam.rehaimi@gmail.com>
2020-02-12 22:44:04 +0100

    edites

--- src/app/parametre/parametre.component.ts
+++ src/app/parametre/parametre.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { User } from '../models/model.user';
 
 @Component({
   selector: 'app-parametre',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParametreComponent implements OnInit {
 
-  constructor() { }
+  private password: string;
+  private confpass: string;
+  private newpass: string;
+  private email: string;
+  private user: User = {
+    nom: '',
+    prenom: '',
+    email: '',
+    password: '',
+    ville: '',
+    adress: '',
+    tel: ''
+  };
+
+  constructor(private service: UserService) { }
 
   ngOnInit() {
+    this.service.findUser(localStorage.getItem('id')).subscribe(
+      (user: User) => {
+        this.password = user.password;
+      }
+    );
+  }
+
+  updatePassword(){
+    this.user = this.service.form.value;
+    console.log(this.user.password);
+
+    this.service.findUser(localStorage.getItem('id')).subscribe(
+      (user: User) => {
+        this.user = user;
+        if((this.confpass == this.newpass) && (this.password == user.password)){
+          user.password = this.user.password;
+          console.log(user.password );
+          //this.service.update(user).subscribe();
+          console.log("ok");
+        }else{
+          console.log("Not identique !!")
+        }
+      }
+    );
+  }
+
+
+  updateEmail(){
+    this.user = this.service.form.value;
+
+    console.log(this.email)
+    this.service.findUser(localStorage.getItem('id')).subscribe(
+      (user: User) => {
+        user.email = this.user.email;
+        this.service.update(user).subscribe();
+      }
+    );
   }
 
 }
